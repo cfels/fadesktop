dd9f2d5717e565773f8f5651ca2406acd5945ccd
Burhanverse <contact@burhanverse.eu.org>
2026-09-17 15:58:12 +0530

    '2.5.8.beta 1' -> '2.5.8.beta 2'
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -25,5 +25,5 @@ constexpr auto AppFile = "FAgram"_cs;
 constexpr auto AppVersion = 2005008;
 constexpr auto AppVersionStr = "2.5.8";
 constexpr auto AppBetaVersion = true;
-constexpr auto AppBetaNumber = 1;
+constexpr auto AppBetaNumber = 2;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
